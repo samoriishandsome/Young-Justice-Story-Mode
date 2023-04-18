444b239123ff1163a714b4bf7c1f3a401adbfa91
S-and-Jsand <18008733-S-and-Jsand@users.noreply.replit.com>
2023-04-18 13:19:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let travelButton=document.querySelector(".travel-one");
+let travelButton=document.querySelector(".travel-two");
+let thirdButton=document.querySelector(".option-three");
+let fourthButton=document.querySelector(".option-four");
+let firstAnswer=document.querySelector(".outcome-one-screen");
+let secondAnswer=document.querySelector(".outcome-two-screen");
+let thirdAnswer=document.querySelector(".outcome-three-screen");
+let fourthAnswer=document.querySelector(".outcome-four-screen");
+
+let storyOpening=document.querySelector(".story-opening");
+let optionOneScreen=document.querySelector(".option-one-screen");
+let optionTwoScreen=document.querySelector(".option-two-screen");
+let optionThreeScreen=document.querySelector(".option-three-screen");
+let optionFourthScreen=document.querySelector(".option-four-screen");
+
+firstOption.onclick=function(){
+  firstOutcome.style.display="button";
+
+};
+
+
+
+secondOption.onclick=function(){
+  secondOutcome.style.display="button";
+
+};
+
+
+thirdOption.onclick=function(){
+  thirdOutcome.style.display="button";
+
+};
+
+
+fourthOption.onclick=function(){
+  fourthOutcome.style.display="button";
+
+};
+
+
